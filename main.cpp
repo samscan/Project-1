3dba03f6a00e751808e588735ac69048cf2091ac
samscan <scanlons@union.edu>
2024-01-16 18:48:16 +0000

    https://github.com/samscan/Project-1

--- main.cpp
+++ main.cpp
@@ -1,23 +1,45 @@
 #include "mbed.h"
 #include "arm_book_lib.h"
 
+DigitalIn driverSeat(D2);
+DigitalIn driverSeatBelt(D3);
+DigitalIn passengerSeat(D4);
+DigitalIn passengerSeatBelt(D5);
+DigitalIn ignition(D6);
+DigitalOut ignitionEnabled(LED1);
+DigitalOut engineStarted(LED1);
+UnbufferedSerial uartUsb(USBTX, USBRX, 115200);
+
+void inputsInit();
+void outputsInit();
+void updateDriverSitting();
+void updateReadyToDrive();
+void updateEngineStarted();
+void uartTask();
+
 int main()
 {
-    DigitalIn gasDetector(D2);
-
-    DigitalOut alarmLed(LED1);
-
-    gasDetector.mode(PullDown);
-
-    alarmLed = OFF;
+    inputsInit();
+    outputsInit();
 
     while (true) {
-        if ( gasDetector == ON ) {
-            alarmLed = ON;
-        }
-        
-        else {
-            alarmLed = OFF;
+        updateDriverSitting();
+
         }
     }
+}
+
+void inputsInit()
+{
+    driverSeat.mode(PullDown);
+    driverSeatBelt.mode(PullDown);
+    passengerSeat.mode(PullDown);
+    passengerSeatBelt.mode(PullDown);
+    ignition.mode(PullDown);
+}
+
+void outputsInit()
+{
+    ignitionEnabled = OFF;
+    engineStarted = OFF;
 }
